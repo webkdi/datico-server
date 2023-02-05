7dbefd42466407b9c49277491f383a7b8ff7e010
webkdi <dimitri.korenev@gmail.com>
2023-02-05 23:57:12 +0100

    werq

--- app.js
+++ app.js
@@ -13,6 +13,22 @@ const app = express();
 app.use(helmet());
 const PORT = process.env.PORT || 3000;
 
+
+var allowedOrigins = ['http://localhost:3000','http://localhost:8080','http://freud.online'];
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
+
 app.use(cors({
   origin: "http://localhost:3000",
 }));
