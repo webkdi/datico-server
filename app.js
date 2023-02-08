58130959a49022402b58c51a08d4630df35e7d8a
webkdi <dimitri.korenev@gmail.com>
2023-02-08 19:51:01 +0100

    wqeqw

--- app.js
+++ app.js
@@ -50,30 +50,6 @@ app.listen(PORT, () => {
   console.log("Server starten on PORT 3000");
 });
 
-// let axios = require("axios");
-// var ymUid = "1631455669534331916";
-// var url =
-//   "https://api-metrika.yandex.net/stat/v1/data?ids=61404367&metrics=ym:pv:users &dimensions=ym:pv:URLPath&accuracy=full&proposed_accuracy=false&limit=1000";
-// var ymRequestOptions = {
-//   headers: {
-//     Authorization: "OAuth AQAAAAAKR6bdAAdww3IEN3DdDkkWsuP4j_ygy6E",
-//   },
-//   redirect: "follow",
-// };
-// app.get("/test", async (req, res) => {
-//   axios
-//     .get(url, ymRequestOptions)
-//     .then((response) => {
-//       console.log(response);
-//       res.send(response.data);
-//     })
-//     .catch((error) => {
-//       if (error.response) {
-//         let { status, statusText } = error.response;
-//         console.log(status, statusText);
-//         res.status(status).send(statusText);
-//       } else {
-//         res.status(404).send(error);
-//       }
-//     });
-// });
+app.get('/testing', (req, res) => {
+  res.send('Hello World!')
+})
