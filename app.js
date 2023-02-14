37114b76746687010c7ec55974993fc2ddbdaa46
Dimitri Ko <dimitri.korenev@gmail.com>
2023-02-14 18:48:55 +0100

    wqeqwre

--- app.js
+++ app.js
@@ -21,10 +21,10 @@ app.use(cors({
     // (like mobile apps or curl requests)
     if(!origin) return callback(null, true);
     if(allowedOrigins.indexOf(origin) === -1){
-      var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
+      var msg = Date().toString() + 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin '+origin;
       return callback(new Error(msg), false);
-    }
+    } 
     return callback(null, true);
   }
 }));
