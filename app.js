0c8e3c5d5871dcc334a5c8d6a867b05dd19e225b
Dimitri Ko <dimitri.korenev@gmail.com>
2023-02-18 22:59:31 +0100

    wqewq

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ app.get('/testing', (req, res) => {
 })
 
 //updates
-var updateStats = cron.schedule('1 1 * * * *', () =>  {
+var updateStats = cron.schedule('1 32 * * * *', () =>  {
   console.log('cron running');
   upd.updateProcedure();
 }, {
