4a030a399b9e7dfa4084479f7b18282e75100e3f
Dimitri Ko <dimitri.korenev@gmail.com>
2023-02-18 18:52:48 +0100

    wqewqe

--- components/Images.js
+++ components/Images.js
@@ -1,38 +1,45 @@
 const fs = require("fs");
 const path = require("path");
+const sharp = require("sharp");
 
 const searchDir = "/var/www/dimitri.korenev/data/www/freud.online/";
 
-function searchForImageFiles(dir, visited, fileList) {
+function searchForImageFiles(dir, fileList) {
   if (!fileList) {
     fileList = [];
   }
-  if (!visited) {
-    visited = new Set();
-  }
-  visited.add(dir);
   const files = fs.readdirSync(dir, { withFileTypes: true });
   files.forEach((file) => {
-    if (file.isDirectory()) {
+    if (file.isDirectory() && !file.name.includes("administrator")) {
       const subDir = path.join(dir, file.name);
-      if (!visited.has(subDir)) {
-        searchForImageFiles(subDir, visited, fileList);
-      }
-    } else {
-      if (file.name.endsWith(".jpg") || file.name.endsWith(".png")) {
-        const filePath = path.join(dir, file.name);
-        const stats = fs.statSync(filePath);
-        const fileSizeInBytes = stats.size;
-        const fileSizeInKB = Math.round(fileSizeInBytes / 1024);
-        const fileType = path.extname(file.name);
-        if (fileSizeInKB > 10 && !filePath.includes('/administrator/')) {
+      searchForImageFiles(subDir, fileList);
+    } else if (
+      (file.name.endsWith(".jpg") || file.name.endsWith(".png")) &&
+      file.size > 30000
+    ) {
+      const filePath = path.join(dir, file.name);
+      const fileSizeInBytes = file.size;
+      const fileSizeInKB = Math.round(fileSizeInBytes / 1024);
+      const fileType = path.extname(file.name);
+
+      const stats = fs.statSync(filePath);
+      sharp(filePath).metadata((err, metadata) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        const width = metadata.width;
+        const height = metadata.height;
+        if (width > 30) {
           fileList.push({
             filePath,
             fileType,
             fileSizeInKB,
+            width,
+            height,
           });
         }
-      }
+      });
     }
   });
   return fileList;
