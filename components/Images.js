572bebcf45f818a37beed6207317a192727e0f7b
Dimitri Ko <dimitri.korenev@gmail.com>
2023-02-18 20:07:33 +0100

    wqewq

--- components/Images.js
+++ components/Images.js
@@ -1,58 +1,49 @@
 const fs = require("fs");
 const path = require("path");
-const sharp = require("sharp");
 
-const mediaDir = "/var/www/dimitri.korenev/data/www/freud.online/media/";
-const imagesDir = "/var/www/dimitri.korenev/data/www/freud.online/images/";
+const baseDir = "/var/www/dimitri.korenev/data/www/freud.online/";
+const searchDirs = ["media", "images"].map((dir) => path.join(baseDir, dir));
 
-function searchForImageFiles(dir, fileList) {
+function searchForImageFiles(dir, visited, fileList) {
   if (!fileList) {
     fileList = [];
   }
-  try {
-    const files = fs.readdirSync(dir, { withFileTypes: true });
-    files.forEach((file) => {
-      if (file.isDirectory() && !file.name.includes("administrator")) {
-        const subDir = path.join(dir, file.name);
-        searchForImageFiles(subDir, fileList);
-      } else if (
-        (file.name.endsWith(".jpg") || file.name.endsWith(".png")) &&
-        (dir === mediaDir || dir === imagesDir) // check if the directory matches mediaDir or imagesDir
-      ) {
+  if (!visited) {
+    visited = new Set();
+  }
+  visited.add(dir);
+  const files = fs.readdirSync(dir, { withFileTypes: true });
+  files.forEach((file) => {
+    if (file.isDirectory()) {
+      const subDir = path.join(dir, file.name);
+      if (!visited.has(subDir) && searchDirs.includes(subDir)) {
+        searchForImageFiles(subDir, visited, fileList);
+      }
+    } else {
+      if (file.name.endsWith(".jpg") || file.name.endsWith(".png")) {
         const filePath = path.join(dir, file.name);
         const stats = fs.statSync(filePath);
         const fileSizeInBytes = stats.size;
         const fileSizeInKB = Math.round(fileSizeInBytes / 1024);
         const fileType = path.extname(file.name);
-
-        sharp(filePath).metadata((err, metadata) => {
-          if (err) {
-            console.log(err);
-            return;
-          }
-          const width = metadata.width;
-          const height = metadata.height;
-          if (fileSizeInKB > 30 && width > 30) {
-            fileList.push({
-              filePath,
-              fileType,
-              fileSizeInKB,
-              width,
-              height,
-            });
-          }
-        });
+        if (fileSizeInKB > 20 && !filePath.includes('/administrator/')) {
+          fileList.push({
+            filePath,
+            fileType,
+            fileSizeInKB,
+          });
+        }
       }
-    });
-  } catch (err) {
-    console.error(`Error reading directory ${dir}:`, err);
-  }
+    }
+  });
   return fileList;
 }
 
 function getListOfImages() {
-  const fileList = searchForImageFiles(mediaDir);
-  fileList.push(...searchForImageFiles(imagesDir));
+  let fileList = [];
+  searchDirs.forEach((dir) => {
+    fileList = fileList.concat(searchForImageFiles(dir));
+  });
   const json = JSON.stringify(fileList, null, 2);
   fs.writeFileSync("image_files.json", json);
   return fileList;
