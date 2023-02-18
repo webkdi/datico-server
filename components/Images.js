7dcb0ff451d58826da99f4ce177550dd2b034e54
Dimitri Ko <dimitri.korenev@gmail.com>
2023-02-18 19:35:24 +0100

    wqeqwe

--- components/Images.js
+++ components/Images.js
@@ -8,40 +8,44 @@ function searchForImageFiles(dir, fileList) {
   if (!fileList) {
     fileList = [];
   }
-  const files = fs.readdirSync(dir, { withFileTypes: true });
-  files.forEach((file) => {
-    if (file.isDirectory() && !file.name.includes("administrator")) {
-      const subDir = path.join(dir, file.name);
-      searchForImageFiles(subDir, fileList);
-    } else if (
-      (file.name.endsWith(".jpg") || file.name.endsWith(".png")) &&
-      file.size > 30000
-    ) {
-      const filePath = path.join(dir, file.name);
-      const fileSizeInBytes = file.size;
-      const fileSizeInKB = Math.round(fileSizeInBytes / 1024);
-      const fileType = path.extname(file.name);
+  try {
+    const files = fs.readdirSync(dir, { withFileTypes: true });
+    files.forEach((file) => {
+      if (file.isDirectory() && !file.name.includes("administrator")) {
+        const subDir = path.join(dir, file.name);
+        searchForImageFiles(subDir, fileList);
+      } else if (
+        (file.name.endsWith(".jpg") || file.name.endsWith(".png")) &&
+        file.size > 30000
+      ) {
+        const filePath = path.join(dir, file.name);
+        const fileSizeInBytes = file.size;
+        const fileSizeInKB = Math.round(fileSizeInBytes / 1024);
+        const fileType = path.extname(file.name);
 
-      const stats = fs.statSync(filePath);
-      sharp(filePath).metadata((err, metadata) => {
-        if (err) {
-          console.log(err);
-          return;
-        }
-        const width = metadata.width;
-        const height = metadata.height;
-        if (width > 30) {
-          fileList.push({
-            filePath,
-            fileType,
-            fileSizeInKB,
-            width,
-            height,
-          });
-        }
-      });
-    }
-  });
+        const stats = fs.statSync(filePath);
+        sharp(filePath).metadata((err, metadata) => {
+          if (err) {
+            console.log(err);
+            return;
+          }
+          const width = metadata.width;
+          const height = metadata.height;
+          if (width > 30) {
+            fileList.push({
+              filePath,
+              fileType,
+              fileSizeInKB,
+              width,
+              height,
+            });
+          }
+        });
+      }
+    });
+  } catch (err) {
+    console.error(`Error reading directory ${dir}:`, err);
+  }
   return fileList;
 }
 
