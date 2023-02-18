aebd380af1e1186f16244077c0b68da5c40f47fc
Dimitri Ko <dimitri.korenev@gmail.com>
2023-02-18 19:10:00 +0100

    wqewqe

--- components/Images.js
+++ components/Images.js
@@ -1,9 +1,10 @@
 const fs = require("fs");
 const path = require("path");
+const Jimp = require("jimp");
 
 const searchDir = "/var/www/dimitri.korenev/data/www/freud.online/";
 
-function searchForImageFiles(dir, visited, fileList) {
+async function searchForImageFiles(dir, visited, fileList) {
   if (!fileList) {
     fileList = [];
   }
@@ -12,37 +13,40 @@ function searchForImageFiles(dir, visited, fileList) {
   }
   visited.add(dir);
   const files = fs.readdirSync(dir, { withFileTypes: true });
-  files.forEach((file) => {
-    if (file.isDirectory()) {
+  for (const file of files) {
+    if (file.isDirectory() && !file.name.includes("administrator")) {
       const subDir = path.join(dir, file.name);
       if (!visited.has(subDir)) {
-        searchForImageFiles(subDir, visited, fileList);
+        await searchForImageFiles(subDir, visited, fileList);
       }
-    } else {
-      if (file.name.endsWith(".jpg") || file.name.endsWith(".png")) {
-        const filePath = path.join(dir, file.name);
-        const stats = fs.statSync(filePath);
-        const fileSizeInBytes = stats.size;
-        const fileSizeInKB = Math.round(fileSizeInBytes / 1024);
-        const fileType = path.extname(file.name);
-        if (fileSizeInKB > 20 && !filePath.includes('/administrator/')) {
+    } else if (file.isFile() && (file.name.endsWith(".jpg") || file.name.endsWith(".png"))) {
+      const filePath = path.join(dir, file.name);
+      const stats = fs.statSync(filePath);
+      const fileSizeInBytes = stats.size;
+      const fileSizeInKB = Math.round(fileSizeInBytes / 1024);
+      if (fileSizeInKB > 30) {
+        const image = await Jimp.read(filePath);
+        const width = image.bitmap.width;
+        const height = image.bitmap.height;
+        if (width > 30) {
           fileList.push({
             filePath,
-            fileType,
+            fileType: path.extname(file.name),
             fileSizeInKB,
+            width,
+            height
           });
         }
       }
     }
-  });
+  }
   return fileList;
 }
 
-function getListOfImages() {
-  const fileList = searchForImageFiles(searchDir);
+async function getListOfImages() {
+  const fileList = await searchForImageFiles(searchDir);
   const json = JSON.stringify(fileList, null, 2);
   fs.writeFileSync("image_files.json", json);
-  // console.log('List of found image files:', fileList);
   return fileList;
 }
 
