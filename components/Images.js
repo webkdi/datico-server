daa0622f2c0d3876fca8035579cd3699445c03c5
Dimitri Ko <dimitri.korenev@gmail.com>
2023-02-18 22:23:26 +0100

    wqewq

--- components/Images.js
+++ components/Images.js
@@ -70,23 +70,24 @@ function optimizeImage(path) {
   const maxHeight = 600;
 
   // Use the sharp library to resize the image
-  sharp(imagePath, (err, info) => {
-    if (err) {
-      console.error(err);
-    } else if (info.format) {
-      sharp(imagePath)
-        .resize(maxWidth, maxHeight, { fit: sharp.fit.inside })
-        .toFile(imagePath, (err, info) => {
-          if (err) {
-            console.error(err);
-          } else {
-            console.log(info);
-          }
-        });
-    } else {
-      console.error(`Invalid input: ${imagePath} is not a valid image file`);
-    }
-  });
+  sharp(imagePath)
+    .resize(maxWidth, maxHeight, { fit: sharp.fit.inside })
+    .toBuffer((err, buffer, info) => {
+      if (err) {
+        console.error(err);
+      } else if (info.format) {
+        sharp(buffer)
+          .toFile(imagePath, (err, info) => {
+            if (err) {
+              console.error(err);
+            } else {
+              console.log(info);
+            }
+          });
+      } else {
+        console.error(`Invalid input: ${imagePath} is not a valid image file`);
+      }
+    });
 }
 
 module.exports = {
