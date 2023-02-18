b9dc6feeb700865b8d9b75f0429cb7c416114e40
Dimitri Ko <dimitri.korenev@gmail.com>
2023-02-18 20:03:22 +0100

    wqewqe

--- components/Images.js
+++ components/Images.js
@@ -1,48 +1,60 @@
 const fs = require("fs");
 const path = require("path");
+const sharp = require("sharp");
 
-const searchDir = "/var/www/dimitri.korenev/data/www/freud.online/";
+const mediaDir = "/var/www/dimitri.korenev/data/www/freud.online/media/";
+const imagesDir = "/var/www/dimitri.korenev/data/www/freud.online/images/";
 
-function searchForImageFiles(dir, visited, fileList) {
+function searchForImageFiles(dir, fileList) {
   if (!fileList) {
     fileList = [];
   }
-  if (!visited) {
-    visited = new Set();
-  }
-  visited.add(dir);
-  const files = fs.readdirSync(dir, { withFileTypes: true });
-  files.forEach((file) => {
-    if (file.isDirectory()) {
-      const subDir = path.join(dir, file.name);
-      if (!visited.has(subDir)) {
-        searchForImageFiles(subDir, visited, fileList);
-      }
-    } else {
-      if (file.name.endsWith(".jpg") || file.name.endsWith(".png")) {
+  try {
+    const files = fs.readdirSync(dir, { withFileTypes: true });
+    files.forEach((file) => {
+      if (file.isDirectory() && !file.name.includes("administrator")) {
+        const subDir = path.join(dir, file.name);
+        searchForImageFiles(subDir, fileList);
+      } else if (
+        (file.name.endsWith(".jpg") || file.name.endsWith(".png")) &&
+        (dir === mediaDir || dir === imagesDir) // check if the directory matches mediaDir or imagesDir
+      ) {
         const filePath = path.join(dir, file.name);
         const stats = fs.statSync(filePath);
         const fileSizeInBytes = stats.size;
         const fileSizeInKB = Math.round(fileSizeInBytes / 1024);
         const fileType = path.extname(file.name);
-        if (fileSizeInKB > 20 && !filePath.includes('/administrator/')) {
-          fileList.push({
-            filePath,
-            fileType,
-            fileSizeInKB,
-          });
-        }
+
+        sharp(filePath).metadata((err, metadata) => {
+          if (err) {
+            console.log(err);
+            return;
+          }
+          const width = metadata.width;
+          const height = metadata.height;
+          if (fileSizeInKB > 30 && width > 30) {
+            fileList.push({
+              filePath,
+              fileType,
+              fileSizeInKB,
+              width,
+              height,
+            });
+          }
+        });
       }
-    }
-  });
+    });
+  } catch (err) {
+    console.error(`Error reading directory ${dir}:`, err);
+  }
   return fileList;
 }
 
 function getListOfImages() {
-  const fileList = searchForImageFiles(searchDir);
+  const fileList = searchForImageFiles(mediaDir);
+  fileList.push(...searchForImageFiles(imagesDir));
   const json = JSON.stringify(fileList, null, 2);
   fs.writeFileSync("image_files.json", json);
-  // console.log('List of found image files:', fileList);
   return fileList;
 }
 
