d10519720163e0c2969e891f3c9d6e32d0545edd
Dimitri Ko <dimitri.korenev@gmail.com>
2023-07-17 10:50:39 +0200

    eqw4

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const helmet = require("helmet");
-const cors= require("cors");
-var cron = require('node-cron');
-const upd = require("./components/UpdateFunctions")
+const cors = require("cors");
+var cron = require("node-cron");
+const upd = require("./components/UpdateFunctions");
 const img = require("./components/ImagesFunctions");
 const tg = require("./components/TelegramFunctions");
 const sb = require("./components/SaleBot");
@@ -19,21 +19,33 @@ const app = express();
 app.use(helmet());
 const PORT = process.env.PORT || 10000;
 
-
-var allowedOrigins = ['http://localhost:3000','http://localhost:3001','http://localhost:8080','https://freud.online','https://app.freud.online','http://194.67.105.122','http://89.108.103.129'];
-app.use(cors({
-  origin: function(origin, callback){
-    // allow requests with no origin 
-    // (like mobile apps or curl requests)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      var msg = Date().toString() + 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin '+origin;
-      return callback(new Error(msg), false);
-    } 
-    return callback(null, true);
-  }
-}));
+var allowedOrigins = [
+  "http://localhost:3000",
+  "http://localhost:3001",
+  "http://localhost:8080",
+  "https://freud.online",
+  "https://app.freud.online",
+  "http://194.67.105.122",
+  "http://89.108.103.129",
+];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin
+      // (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg =
+          Date().toString() +
+          "The CORS policy for this site does not " +
+          "allow access from the specified Origin " +
+          origin;
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
@@ -60,38 +72,57 @@ app.listen(PORT, () => {
   console.log(`Server starten on PORT ${PORT}`);
 });
 
-app.get('/testing', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/testing", (req, res) => {
+  res.send("Hello World!");
+});
 
 //updates
-var updateStats = cron.schedule('1 35 * * * *', () =>  {
-  console.log('cron undate stat running running');
-  upd.updateProcedure();
-}, {
-  scheduled: false
-});
-if (process.env.THIS_VERSION === 'LIVE') {updateStats.start();}
+var updateStats = cron.schedule(
+  "1 35 * * * *",
+  () => {
+    console.log("cron undate stat running running");
+    upd.updateProcedure();
+  },
+  {
+    scheduled: false,
+  }
+);
+if (process.env.THIS_VERSION === "LIVE") {
+  updateStats.start();
+}
 // updateStats.start();
 
 //optimize new images
 //once per week on every Saturday at 2:30 am
-var updateImages = cron.schedule('1 30 2 * * 6', () =>  {
-  console.log('cron optimize images running');
-  img.dailyImageService();
-}, {
-  scheduled: false
-});
-if (process.env.THIS_VERSION === 'LIVE') {updateImages.start();}
+var updateImages = cron.schedule(
+  "1 30 2 * * 6",
+  () => {
+    console.log("cron optimize images running");
+    img.dailyImageService();
+  },
+  {
+    scheduled: false,
+  }
+);
+if (process.env.THIS_VERSION === "LIVE") {
+  updateImages.start();
+}
 
 //Telefram Reposting
-var reportTgToFb = cron.schedule('15 1 * * * *' , () =>  {
-// var reportTgToFb = cron.schedule('*/15 * * * * *' , () =>  {
-  tg.infoDefRepost();
-}, {
-  scheduled: false
-});
-if (process.env.THIS_VERSION === 'LIVE') {reportTgToFb.start();}
+var reportTgToFb = cron.schedule(
+  "15 1 * * * *",
+  () => {
+    // var reportTgToFb = cron.schedule('*/15 * * * * *' , () =>  {
+    tg.infoDefRepost();
+  },
+  {
+    scheduled: false,
+  }
+);
+if (process.env.THIS_VERSION === "LIVE") {
+  console.log("cron reportTgToFb started");
+  reportTgToFb.start();
+}
 // reportTgToFb.start();
 
 // # ┌────────────── second (optional)
