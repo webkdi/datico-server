01f22a73ecf2bb6afab8f38adfc2e99c0d9e1e86
Dimitri Ko <dimitri.korenev@gmail.com>
2023-11-11 14:18:47 +0100

    eqwqwe

--- components/ImagesFunctions.js
+++ components/ImagesFunctions.js
@@ -104,7 +104,7 @@ function optimizeImage(path, password) {
         dither: 0.5,
         trellisQuantisation: true,
         quantisationPosterize: 2,
-        background: { r: 255, g: 255, b: 255, alpha: 0 },
+        // background: { r: 255, g: 255, b: 255, alpha: 0 },
       })
       .jpeg({ quality: 60 })
       .toBuffer((err, buffer, info) => {
