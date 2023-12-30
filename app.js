cee4d37e256029f26c28867a577209e270249a78
Dimitri <dimitri.korenev@gmail.com>
2023-12-30 19:16:05 +0100

    sada

--- app.js
+++ app.js
@@ -138,7 +138,7 @@ if (process.env.THIS_VERSION === "LIVE") {
 
 //GoogleNewsAT reposting
 var schnitzelNewsBot = cron.schedule(
-  '42 */1 * * * *',
+  '42 */2 * * * *',
   () => {
     glNews.parseGoogleNewsRss();
   },
