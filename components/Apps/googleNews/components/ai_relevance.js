ecbaae289cfbe0b819c315adf193efef1c86cbe7
Dimitri <dimitri.korenev@gmail.com>
2024-01-09 10:44:49 +0100

    wewq

--- components/Apps/googleNews/components/ai_relevance.js
+++ components/Apps/googleNews/components/ai_relevance.js
@@ -2,12 +2,13 @@ const axios = require('axios');
 require('dotenv').config();
 
 // const relProject = process.env.RELEVANCE_PROJECT;
-const relProject = 'd29d7a1698e0-44d3-bb75-d756c1cd3b3c';
+const relProject = 'd5ee5eaebddc-40f6-8cd9-589323c0815a';
+const relApiUrl = 'https://api-d7b62b.stack.tryrelevance.com/latest/studios/85e86411-4331-4fdb-8ec7-8150153ed293/trigger_limited';
 
 // Async function to perform the POST request
 async function triggerRelAi(text) {
     console.log("Длина вводного текста в Relevance:", text.length);
-    const url = "https://api-d7b62b.stack.tryrelevance.com/latest/studios/c8632020-f831-4f65-9a72-9124abeb4d83/trigger_limited";
+    const url = relApiUrl;
     const data = {
         params: {
             text: text
@@ -35,8 +36,6 @@ async function triggerRelAi(text) {
     }
 }
 // triggerLimitedAPI(message);
-
-
 
 module.exports = {
     triggerRelAi,
