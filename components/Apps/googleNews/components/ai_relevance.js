1249746ac5e0afda9b17f7105612b4d24f7b653c
Dimitri <dimitri.korenev@gmail.com>
2024-01-17 09:58:26 +0100

    wqewqe

--- components/Apps/googleNews/components/ai_relevance.js
+++ components/Apps/googleNews/components/ai_relevance.js
@@ -2,8 +2,8 @@ const axios = require('axios');
 require('dotenv').config();
 
 // const relProject = process.env.RELEVANCE_PROJECT;
-const relProject = 'd5ee5eaebddc-40f6-8cd9-589323c0815a';
-const relApiUrl = 'https://api-d7b62b.stack.tryrelevance.com/latest/studios/85e86411-4331-4fdb-8ec7-8150153ed293/trigger_limited';
+const relProject = '2eef9c6b19ed-46e9-9f2b-9c93e11583a8';
+const relApiUrl = 'https://api-d7b62b.stack.tryrelevance.com/latest/studios/cfb8606f-4af4-457d-84f0-d21b05940e0f/trigger_limited';
 
 // Async function to perform the POST request
 async function triggerRelAi(text) {
